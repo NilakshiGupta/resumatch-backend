7bbfff53c61504aacb5e88f93f2d737cd6f04a2f
Nilakshi <155097474+ishkalin@users.noreply.github.com>
2026-04-26 22:01:42 +0530

    initial commit

---
+++ src/test/java/reume/matcher/MatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package reume.matcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
